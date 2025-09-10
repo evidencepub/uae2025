8443cc7e9dca97d2cc8eef8298ae853cd3b52024
agahkarakuzu <agahkarakuzu@gmail.com>
2025-09-10 13:28:05 +0300

    Add carousel

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+const images = document.querySelectorAll('.carousel img');
+let current = 0;
+
+function showNextImage() {
+  images[current].classList.remove('active');
+  current = (current + 1) % images.length;
+  images[current].classList.add('active');
+}
+
+setInterval(showNextImage, 3000);
